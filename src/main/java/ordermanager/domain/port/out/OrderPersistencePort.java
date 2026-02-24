3242968ec52bec8bf3111df03162ec164cccb596
shadKalhor <shad.jalal.aziz@gmail.com>
2026-02-24 13:42:13 +0300

    Changed OrderPersistencePort.java to implement these new Return Types save -> Either<StructuredError, Entity> GetAll -> List<Entity> GetALlByUser -> List<Entity> Get -> Option<Entity> Delete -> Either<StructuredError, Void>

--- src/main/java/ordermanager/domain/port/out/OrderPersistencePort.java
+++ src/main/java/ordermanager/domain/port/out/OrderPersistencePort.java
@@ -1,19 +1,21 @@
 package ordermanager.domain.port.out;
 
+import io.vavr.control.Either;
 import io.vavr.control.Option;
+import ordermanager.domain.exception.StructuredError;
 import ordermanager.infrastructure.store.persistence.entity.Order;
 
 import java.util.List;
 import java.util.UUID;
 
 public interface OrderPersistencePort {
-    Option<Order> save(Order order);
+    Either<StructuredError, Order> save(Order order);
 
     List<Order> findAll();
 
     Option<Order> findById(UUID orderId);
 
-    void deleteById(UUID orderId);
+    Either<StructuredError, Void> deleteById(UUID orderId);
 
-    Option<List<Order>> findByUserId(UUID userId);
+    List<Order> findByUserId(UUID userId);
 }
