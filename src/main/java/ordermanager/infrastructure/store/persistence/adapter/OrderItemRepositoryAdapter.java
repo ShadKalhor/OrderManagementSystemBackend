4c4a43847eb1ed5e04e77ab1e81710f58a2dca1f
shadKalhor <shad.jalal.aziz@gmail.com>
2026-04-08 18:23:49 +0300

    init

--- src/main/java/ordermanager/infrastructure/store/persistence/adapter/OrderItemRepositoryAdapter.java
+++ src/main/java/ordermanager/infrastructure/store/persistence/adapter/OrderItemRepositoryAdapter.java
@@ -1,8 +1,23 @@
 package ordermanager.infrastructure.store.persistence.adapter;
 
+import io.vavr.control.Option;
+import ordermanager.domain.model.OrderItemDomain;
+import ordermanager.domain.port.out.OrderItemPersistencePort;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public class OrderItemRepositoryAdapter {
+import java.util.List;
+import java.util.UUID;
 
+@Repository
+public class OrderItemRepositoryAdapter implements OrderItemPersistencePort {
+
+    @Override
+    public List<OrderItemDomain> GetOrderItemsByIds(List<UUID> orderItemIds) {
+        return List.of();
+    }
+
+    @Override
+    public Option<OrderItemDomain> GetById(UUID orderItemId) {
+        return null;
+    }
 }
