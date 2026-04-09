ee807a19d571050a184b5fc9eb0ebc57879f7e26
shadKalhor <shad.jalal.aziz@gmail.com>
2026-04-09 11:04:54 +0300

    Added Response.

--- src/contractTest/resources/contracts/address/should_create_address.groovy
+++ src/contractTest/resources/contracts/address/should_create_address.groovy
@@ -1,4 +1,5 @@
 package contracts.address
+
 import org.springframework.cloud.contract.spec.Contract
 
 Contract.make {
@@ -15,14 +16,28 @@ Contract.make {
                 name: "addressName",
                 city: "erbil",
                 description: "my Description",
-                type: "house",
-                street: "405",
-                residentialNo: "102",
+                type: "House",
+                street: "1st Street",
+                residentialNo: "405",
                 isPrimary: true
         )
     }
 
     response {
         status CREATED()
+        headers {
+            contentType(applicationJson())
+        }
+        body(
+                id: "22222222-2222-2222-2222-222222222222",
+                userId: "112961f9-462e-49bd-85dd-b56041ec65d2",
+                name: "addressName",
+                city: "erbil",
+                description: "my Description",
+                type: "House",
+                street: "1st Street",
+                residentialNo: "405",
+                isPrimary: true
+        )
     }
 }
